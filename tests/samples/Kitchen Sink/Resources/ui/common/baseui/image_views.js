e2be55eae700165c841f18f5950cfca52c86b4d9
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2012-11-15 19:28:09 +0200

    fixed Animated item in Kitchen Sink (BaseUI->Views->Image Views->Animated)

--- tests/samples/Kitchen Sink/Resources/ui/common/baseui/image_views.js
+++ tests/samples/Kitchen Sink/Resources/ui/common/baseui/image_views.js
@@ -9,8 +9,11 @@ function image_views(_args) {
 		{title:'Image View Positioning', hasChild:true, test:'ui/common/baseui/image_view_positioning'}
 	];
 	
-	if (!(Ti.Platform.osname === 'mobileweb' || Ti.Platform.osname === 'tizen')) {
+	if (!(Ti.Platform.osname === 'mobileweb')) {
 		data.push({title:'Animated', hasChild:true, test:'ui/common/baseui/image_view_animated'});
+	}
+	
+	if (!(Ti.Platform.osname === 'mobileweb' || Ti.Platform.osname === 'tizen')) {
 		data.push({title:'Image View Encoding', hasChild:true, test:'ui/common/baseui/image_view_encoding'});
 	}
 	
