6574e161225f6f9e55e90dada70cba3c3acbb7de
ivan.moroz <ivan.moroz@globallogic.com>
2012-12-17 13:02:23 +0200

    Added normal view for pickers in tizen, changed position absolute to relative in Ti.UI.Picker.js

--- tests/samples/KitchenSink/Resources/ui/common/controls/picker_custom2.js
+++ tests/samples/KitchenSink/Resources/ui/common/controls/picker_custom2.js
@@ -2,7 +2,11 @@ function picker_custom2() {
 	var win = Ti.UI.createWindow();
 	win.backgroundColor = 'black';
 	
-	var picker = Ti.UI.createPicker();
+	var picker = Ti.UI.createPicker({
+		width: '100',
+		height: '110',
+		color: '#fc0'
+	});
 	
 	function addRow(x)
 	{
