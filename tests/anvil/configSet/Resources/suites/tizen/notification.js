13f9c107a78e5ce7ccb032b51c5d6a6651d000ef
Anatoliy Odukha <aodukha@gmail.com>
2012-12-21 20:15:39 +0200

    missed files, just a stub

---
+++ tests/anvil/configSet/Resources/suites/tizen/notification.js
@@ -0,0 +1,26 @@
+/*
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+module.exports = new function() {
+	var finish;
+	var valueOf;
+	this.init = function(testUtils) {
+		finish = testUtils.finish;
+		valueOf = testUtils.valueOf;
+	}
+
+	this.name = "notifications";
+	this.tests = [
+		{name: "test1"}
+	]
+
+	this.test1 = function(testRun) {
+		console.log('Kto zakomital notification and call testy v app.js no ne dobavil eti files? Iz-za vas ves suit padal');
+		valueOf(testRun, 1).shouldBeNumber();
+		finish(testRun);
+	}
+}
