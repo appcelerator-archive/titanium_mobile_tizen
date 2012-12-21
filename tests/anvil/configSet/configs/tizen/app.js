1afd7bcab7a3d6c5ba3c7945201f02013b4c99fd
Anatoliy Odukha <aodukha@gmail.com>
2012-12-21 20:02:23 +0200

    dissabled absent tests

--- tests/anvil/configSet/configs/tizen/app.js
+++ tests/anvil/configSet/configs/tizen/app.js
@@ -7,12 +7,11 @@
 
 // start customization here
 var suites = [
-	{name: "tizen/media_content"}
 	{name: "tizen/media_content"},
 	{name: "tizen/alarm"},
-	{name: "tizen/notification"},
+	//{name: "tizen/notification"},
 	{name: "tizen/lbs"},
-	{name: "tizen/call"}
+	//{name: "tizen/call"},
 	{name: "tizen/bluetooth"}
 ];
 
