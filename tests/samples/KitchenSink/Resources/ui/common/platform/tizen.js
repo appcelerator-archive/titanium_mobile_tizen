9a67ece3a856fe14caa35593553d06904f1143e2
ihor-r <ihor.rohatskyi@globallogic.com>
2013-01-21 16:43:36 +0200

    Add Call samples to KitchenSink for Tizen platform.

--- tests/samples/KitchenSink/Resources/ui/common/platform/tizen.js
+++ tests/samples/KitchenSink/Resources/ui/common/platform/tizen.js
@@ -2,6 +2,7 @@ function tizen(_args) {
 	var self = Titanium.UI.createWindow(),
 		data = [
 			{title: 'Contacts', hasChild: true, test: 'ui/handheld/tizen/platform/tizen_contacts'},
+			{title: 'Call', hasChild: true, test: 'ui/handheld/tizen/platform/call/call'},
 			{title: 'Geocoder', hasChild: true, test: 'ui/handheld/tizen/platform/geocoder'},		
 			{title: 'Application', hasChild: true, test: 'ui/common/platform/tizen_application'},
 			{title: 'Filesystem', hasChild: true, test: 'ui/common/platform/tizen_filesystem'},
