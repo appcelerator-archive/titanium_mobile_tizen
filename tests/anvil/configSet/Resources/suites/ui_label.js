f92344079da7693fc7db6b75ceb9f4413eb932e7
igorostash <igor.ostash@globallogic.com>
2013-01-25 14:01:31 +0200

    added test for UI.Label

--- tests/anvil/configSet/Resources/suites/ui_label.js
+++ tests/anvil/configSet/Resources/suites/ui_label.js
@@ -20,7 +20,11 @@ module.exports = new function() {
 	this.name = "ui_label";
 	this.tests = (function(){
 		var arr = [
-			{name: "testProperties"}
+			{name: "testProperties"},
+			{name: "testTextId"},
+			{name: "testElipsize"},
+			{name: "testWordWrap"},
+			{name: "testVerticalAlign"}
 		]
 
 		if(Ti.Platform.osname === 'tizen' || Ti.Platform.osname === 'mobileweb') {
@@ -216,6 +220,177 @@ module.exports = new function() {
 			valueOf(testRun, count).shouldBeGreaterThan(backgroundPixelsCount);
 			win.close();
 			finish(testRun);
+		}
+	}
+
+	this.testTextId = function(testRun) {
+
+		var win = Ti.UI.createWindow({
+			backgroundColor: 'white'
+		});
+
+		var test_text = 'this is label text'
+
+		var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'test.txt');
+		file.write(test_text);
+
+		var label1 = Ti.UI.createLabel({
+			textid: file.nativePath,
+			top: 30,
+			width: 'auto', height: 'auto'
+		});
+				
+		label1.addEventListener('postlayout', checkText);
+
+		win.add(label1);
+		win.addEventListener('close', function(){
+			finish(testRun);
+		})
+
+		win.open();
+
+		function checkText(){
+			var dom_node = label1.domNode;
+			var text_view = dom_node.getElementsByClassName('TiUIView')[0];
+			var text = text_view.innerHTML;
+			valueOf(testRun, text).shouldBe(test_text);
+			//win.close();
+		}
+	}
+
+	this.testElipsize = function(testRun) {
+
+		var win = Ti.UI.createWindow({
+			backgroundColor: 'white'
+		});
+
+		var label1 = Ti.UI.createLabel({
+			text: 'very very long text',
+			top: 30,
+			ellipsize: true,
+			width: '60', height: 'auto'
+		});
+				
+		label1.addEventListener('postlayout', checkLabel);
+
+		win.add(label1);
+		win.addEventListener('close', function(){
+			finish(testRun);
+		})
+
+		win.open();
+
+		function checkLabel(){
+			var dom_node = label1.domNode;
+			var text_view = dom_node.getElementsByClassName('TiUIView')[0];
+			valueOf(testRun, text_view.style.textOverflow).shouldBe('ellipsis');
+			win.close();
+		}
+	}
+
+	this.testWordWrap = function(testRun) {
+
+		var win = Ti.UI.createWindow({
+			backgroundColor: 'white'
+		});
+
+		var label1 = Ti.UI.createLabel({
+			text: 'very very long text',
+			top: 30,
+			wordWrap: false,
+			width: '60', height: 'auto'
+		});
+				
+		label1.addEventListener('postlayout', checkLabel);
+
+		win.add(label1);
+		win.addEventListener('close', function(){
+			finish(testRun);
+		})
+
+		win.open();
+
+		function checkLabel(){
+			var dom_node = label1.domNode;
+			var text_view = dom_node.getElementsByClassName('TiUIView')[0];
+			valueOf(testRun, text_view.style.whiteSpace).shouldBe('nowrap');
+			win.close();
+		}
+	}
+
+	this.testVerticalAlign = function(testRun) {
+
+		var win = Ti.UI.createWindow({
+			backgroundColor: 'white',
+			layout: 'vertical'
+		}),
+
+		label1 = Ti.UI.createLabel({
+			top: 10,
+			width: 160,
+			height: 50,
+			backgroundColor: '#ff55ff',
+			verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_BOTTOM,
+			text: 'some text'
+		}),
+		label2 = Ti.UI.createLabel({
+			top: 10,
+			width: 160,
+			height: 50,
+			backgroundColor: '#ff55ff',
+			verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_CENTER,
+			text: 'some text'
+		}),
+		label3 = Ti.UI.createLabel({
+			top: 10,
+			width: 160,
+			height: 50,
+			backgroundColor: '#ff55ff',
+			verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_TOP,
+			text: 'some text'
+		});
+
+		label1.addEventListener('postlayout', function(){
+			checkLabel(label1, checkAlignBottom);
+		});
+		label1.addEventListener('postlayout', function(){
+			checkLabel(label2, checkAlignCenter);
+		});
+		label1.addEventListener('postlayout', function(){
+			checkLabel(label3, checkAlignTop);
+		});
+
+		win.add(label1);
+		win.add(label2);
+		win.add(label3);
+		win.open();
+
+		win.addEventListener('close', function(){
+			finish(testRun);
+		})
+
+		function checkAlignBottom(view_top, view_bottom){
+			valueOf(testRun, view_top).shouldBeGreaterThan(view_bottom);
+		}
+
+		function checkAlignCenter(view_top, view_bottom){
+			valueOf(testRun, Math.abs(view_top - view_bottom)).shouldBeLessThanEqual(1);
+		}
+
+		function checkAlignTop(view_top, view_bottom){
+			valueOf(testRun, view_top).shouldBeLessThan(view_bottom);
+			win.close()
+		}
+
+		function checkLabel(testLabel, callback){
+			var dom_node = testLabel.domNode,
+				text_view = dom_node.getElementsByClassName('TiUIView')[0],
+				view_top = text_view.style.top;
+			view_top = view_top.slice(0,-2);
+			var v_height = text_view.style.height;
+			v_height = v_height.slice(0,-2);
+			var view_bottom = testLabel.height - view_top - v_height;
+			callback(view_top, view_bottom);
 		}
 	}
 }
