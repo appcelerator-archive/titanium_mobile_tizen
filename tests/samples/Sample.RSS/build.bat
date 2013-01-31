3949cef90d0aa4aeaf4c068a0a19be3fb8c70c00
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-01-31 15:35:03 +0200

    Removed build.bat and folder build from RSS sample

--- tests/samples/Sample.RSS/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo INFO: building application
-call titanium build --platform=tizen
-echo INFO: uninstalling old application
-call web-uninstall -i http://appcelerator.com
-echo INFO: Installing application to emulator
-call c:\tizen-sdk\tools\ide\bin\web-run.bat -w build\tizen\tizenapp.wgt -i http://appcelerator.com
