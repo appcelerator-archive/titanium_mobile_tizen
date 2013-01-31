7ab65fbb69c04f6882e2cb6539b8abe5d8cc9354
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-01-31 14:31:14 +0200

    Update error message for Ti.Contacts.save

--- titanium/Ti/Contacts.js
+++ titanium/Ti/Contacts.js
@@ -414,7 +414,7 @@ define(["Ti/_/Evented", "Ti/_/lang"], function(Evented, lang) {
 		save: function(persons) {
 			var addressbook = tizen.contact.getDefaultAddressBook(), i = 0, personsCount = persons.length;
 			for (i = 0; i < personsCount; i++) {
-				if (!persons[i].id) throw new Error('Fuck id');
+				if (!persons[i].id) throw new Error('Person with given id cannot be found');
 				var c = this._mapContactFromTitanium(persons[i])
 				addressbook.update(this._mapContactFromTitanium(persons[i]));
 			}
