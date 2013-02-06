3e2c4fed030e7c1ab6003ea7dca063aaf390c851
Anatoliy Odukha <aodukha@gmail.com>
2013-02-06 19:33:11 +0200

    fixed according to code convention

--- tests/anvil/configSet/Resources/suites/tizen/alarm.js
+++ tests/anvil/configSet/Resources/suites/tizen/alarm.js
@@ -26,7 +26,7 @@ module.exports = new function() {
 	this.alarmAdd = function(testRun) {
 		var alarm,
 			tizenArr,
-			date = new Date(2012, 11, 21, 8, 0);
+			date = new Date(2012, 12, 21, 8, 0);
 
 		tizen.alarm.removeAll();
 		
@@ -49,10 +49,11 @@ module.exports = new function() {
 			alarm1,
 			alarm2, 
 			alarm3,
-			date = new Date(2012, 11, 21, 8, 0);
+			relative_id,
+			absolute_id,
+			date = new Date(2012, 12, 21, 8, 0);
 
-		tizen.alarm.removeAll();
-		
+		tizen.alarm.removeAll();		
 		//Alarm in 10 seconds (relative)
 		alarm = new tizen.AlarmRelative(10);
 		// Set an alarm on December 21st 2012 08:00
@@ -60,8 +61,8 @@ module.exports = new function() {
 		tizen.alarm.add(alarm, "http://tizen.org/alarm-clock");
 		tizen.alarm.add(alarm1, "http://tizen.org/alarm-clock");
 		
-		var relative_id = alarm.id,
-			absolute_id = alarm1.id;
+		relative_id = alarm.id,
+		absolute_id = alarm1.id;
 
 		valueOf(testRun, function(){
 			alarm2 = tizen.alarm.get(relative_id);
@@ -74,27 +75,33 @@ module.exports = new function() {
 		tizen.alarm.removeAll()
 
 		valueOf(testRun, function(){
-			var alarm2 = tizen.alarm.get(absolute_id);
-			var alarm3 = tizen.alarm.get(relative_id);
+			var alarmTmp = tizen.alarm.get(absolute_id);
+			alarmTmp = tizen.alarm.get(relative_id);
 		}).shouldThrowException();
 
 		finish(testRun);
 	}
 	
 	this.alarmRemove = function(testRun) {
-		tizen.alarm.removeAll();
-		//Alarm in 10 seconds (relative)
-		var alarm = new tizen.AlarmRelative(10),
+		var alarm,
+			alarm1,
+			relative_id,
+			alarmArr,
+			absolute_id,
+			date = new Date(2012, 12, 21, 8, 0);
 		
-			// Set an alarm on December 21st 2012 08:00
-			date = new Date(2012, 11, 21, 8, 0),		
+		tizen.alarm.removeAll();
+		alarmArr = tizen.alarm.getAll();
+		valueOf(testRun, alarmArr.length).shouldBe(0);
 
-			alarm1 = new tizen.AlarmAbsolute(date);
-
+		//Alarm in 10 seconds (relative)
+		alarm = new tizen.AlarmRelative(10);
+		alarm1 = new tizen.AlarmAbsolute(date);
 		tizen.alarm.add(alarm, "http://tizen.org/alarm-clock");
 		tizen.alarm.add(alarm1, "http://tizen.org/alarm-clock");
-		var relative_id = alarm.id,
-			absolute_id = alarm1.id;
+		relative_id = alarm.id;
+		absolute_id = alarm1.id;
+
 		valueOf(testRun, function(){
 			tizen.alarm.remove(absolute_id);	
 		}).shouldNotThrowException();
@@ -103,59 +110,56 @@ module.exports = new function() {
 			tizen.alarm.remove(absolute_id);
 		}).shouldThrowException();
 
-		tizen.alarm.removeAll();
-
-		var alarm_arr = tizen.alarm.getAll();
-		valueOf(testRun, alarm_arr.length).shouldBe(0);
-
 		finish(testRun);
 	}
 
 	this.alarmRelative = function(testRun) {
+		var alarm,
+			alarmId,
+			delay = 2 * tizen.alarm.PERIOD_MINUTE,
+			period = tizen.alarm.PERIOD_HOUR;
+
 		tizen.alarm.removeAll();
-		var delay = 2 * tizen.alarm.PERIOD_MINUTE,
-			period = tizen.alarm.PERIOD_HOUR,
-			alarm = new tizen.AlarmRelative(delay, period);
+		alarm = new tizen.AlarmRelative(delay, period);
 		tizen.alarm.add(alarm, "http://tizen.org/alarm-clock");
 		alarmId = alarm.id;
 		valueOf(testRun, function(){
 			var remaining = alarm.getRemainingSeconds();
-		}).shouldNotThrowException()
-		
+		}).shouldNotThrowException();
 		valueOf(testRun, alarm.delay).shouldBe(delay);
 
-		var alarm_from = tizen.alarm.get(alarmId);
+		alarm = tizen.alarm.get(alarmId);
 		valueOf(testRun, function(){
-			var	from_reaming = alarm_from.getRemainingSeconds();
+			var	fromReaming = alarm.getRemainingSeconds();
 		});
-			
-
-		valueOf(testRun, alarm_from.delay).shouldBe(delay);
+		valueOf(testRun, alarm.delay).shouldBe(delay);
 
 		finish(testRun);
 	}
 
 	this.alarmAbsolute = function(testRun) {
 		tizen.alarm.removeAll();
-		var date = new Date(),
+		var alarm,
+			alarmId,
+			remaining,
+			alarmFrom,
+			fromReaming,
+			date = new Date(),
 			time = date.getTime() + 60000,
 			period = tizen.alarm.PERIOD_HOUR;
-		date.setTime(time);		
-		var	alarm1 = new tizen.AlarmAbsolute(date, period);
 		
-		tizen.alarm.add(alarm1, "http://tizen.org/alarm-clock");
-		var alarmId = alarm1.id;
+		date.setTime(time);
+		alarm = new tizen.AlarmAbsolute(date, period);		
+		tizen.alarm.add(alarm, "http://tizen.org/alarm-clock");
+		alarmId = alarm.id;
+		remaining = alarm.getNextScheduledDate();
+		valueOf(testRun, remaining.toDateString()).shouldBe(alarm.date.toDateString());
 
-		var remaining = alarm1.getNextScheduledDate();
-		valueOf(testRun, remaining.toDateString()).shouldBe(alarm1.date.toDateString());
-
-		var alarm_from = tizen.alarm.get(alarmId),
-			from_reaming = alarm_from.getNextScheduledDate();
-
-		valueOf(testRun, from_reaming.toDateString()).shouldBe(date.toDateString());
+		alarmFrom = tizen.alarm.get(alarmId),
+		fromReaming = alarmFrom.getNextScheduledDate();
+		valueOf(testRun, fromReaming.toDateString()).shouldBe(date.toDateString());
 
 		tizen.alarm.removeAll();
-
 		finish(testRun);
 	}
 }
