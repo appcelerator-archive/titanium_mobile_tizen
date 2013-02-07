4c69380c97942674f613bd4b04164c31bd98a491
Anatoliy Odukha <aodukha@gmail.com>
2013-02-07 18:28:56 +0200

    fixed accordig to code convention

--- tests/anvil/configSet/Resources/suites/tizen/bluetooth.js
+++ tests/anvil/configSet/Resources/suites/tizen/bluetooth.js
@@ -4,17 +4,12 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-/**
- Check tizen.bluetooth avalability
- check result for tizen.bluetooth.getDefaultAdapter();
-
-*/
 
 module.exports = new function() {
-	var finish;
-	var valueOf;
-	var reportError;
-	var adapter;
+	var finish,
+		valueOf,
+		reportError,
+		adapter;
 	
 	this.init = function(testUtils) {
 		finish = testUtils.finish;
@@ -58,48 +53,47 @@ module.exports = new function() {
 	}
 
 	this.discoverDevices = function(testRun){
+		var onBluetoothError = function(e) {
+				reportError(testRun, 'Error in discoverDevices test: ' + e.message);
+			},		
+			onStartedReceived = false, //test should receiVe callback "Discovery started" always
+			discoverDevicesWatcher = {
+				onstarted: function() {
+					//Device discovery started.
+					onStartedReceived = true;
+				},
+				ondevicefound: function(device) {
+					//We found something, unit tests really does not expect to have 
+					// any devices available, but it is possible situation
+					//when there is some device nearby
+					valueOf(testRun, device.name).shouldBeObject();
+					valueOf(testRun, device.address).shouldBeObject();
+					valueOf(testRun, onStartedReceived).shouldBeTrue();
+					try {
+						adapter.stopDiscovery(
+							function() {
+								//Device discovery stopped 
+								finish(testRun);
+							},
+							onBluetoothError);
+					} catch (exc) {
+						reportError(testRun, 'Exception in stop ondevicefound after stopDiscovery:' + exc.message);
+					}
+				},
+				ondevicedisappeared: function(address) {
+					//Device disappeared
+					valueOf(testRun, onStartedReceived).shouldBeTrue();
+					finish(testRun);
+				},
+				onfinished: function(devices) {
+					//Info: device not found
+					valueOf(testRun, onStartedReceived).shouldBeTrue();
+					finish(testRun);
+				}
+			};
 		//check is adapter available and powered on
 		valueOf(testRun, adapter).shouldBeObject();
 		valueOf(testRun, adapter.powered).shouldBeTrue();
-		
-		var onBluetoothError = function(e) {
-			reportError(testRun, 'Error in discoverDevices test: ' + e.message);
-		};
-
-		var onStartedReceived = false;//test should receice callback "Discovery started" always
-		var discoverDevicesWatcher = {
-		    onstarted: function() {
-		        //Device discovery started.
-		        onStartedReceived = true;
-		    },
-		    ondevicefound: function(device) {
-		    	//We found something, unit tests really does not expect to have any devices available, but it is possible situation
-		    	//when there is some device nearby
-		        valueOf(testRun, device.name).shouldBeObject();
-		        valueOf(testRun, device.address).shouldBeObject();
-		        valueOf(testRun, onStartedReceived).shouldBeTrue();
-		        try {		            
-					adapter.stopDiscovery(
-						function() {
-							//Device discovery stopped 
-							finish(testRun);
-						},
-						onBluetoothError);
-		        } catch (exc) {
-		            reportError(testRun, 'Exception in stop ondevicefound after stopDiscovery:' + exc.message);
-		        }
-		    },
-		    ondevicedisappeared: function(address) {
-		        //Device disappeared
-		        valueOf(testRun, onStartedReceived).shouldBeTrue();
-		        finish(testRun);
-		    },
-		    onfinished: function(devices) {
-		        //Info: device not found
-		        valueOf(testRun, onStartedReceived).shouldBeTrue();
-		        finish(testRun);
-		    }
-		};
 		try {
 			adapter.discoverDevices(discoverDevicesWatcher, onBluetoothError);
 		} catch (exc) {
@@ -108,12 +102,11 @@ module.exports = new function() {
 	}
 
 	this.bluetoothFindDevice = function(testRun) {
-		valueOf(testRun, adapter).shouldBeObject();
-		valueOf(testRun, adapter.powered).shouldBeTrue();
-
 		var onBluetoothError = function(e) {
 			reportError(testRun, 'Error in bluetoothFindDevice test: ' + e.message);
 		};
+		valueOf(testRun, adapter).shouldBeObject();
+		valueOf(testRun, adapter.powered).shouldBeTrue();
 		try {
 			adapter.getKnownDevices(
 				function(devices){
@@ -129,13 +122,11 @@ module.exports = new function() {
 	}
 
 	this.PowerOff = function(testRun) {
-		valueOf(testRun, adapter).shouldBeObject();
-		valueOf(testRun, adapter.powered).shouldBeTrue();
-
 		var onBluetoothError = function(e) {
 			reportError(testRun, 'Error in powerOn test: ' + e.message);
 		}
-
+		valueOf(testRun, adapter).shouldBeObject();
+		valueOf(testRun, adapter.powered).shouldBeTrue();
 		try {
 			adapter.setPowered(false,			
 				function() {
