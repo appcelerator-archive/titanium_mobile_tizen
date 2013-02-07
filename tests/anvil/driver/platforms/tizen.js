70e42776ced671dd336dd0acbf1f78cdeaa03c79
ihor-r <ihor.rohatskyi@globallogic.com>
2013-02-07 13:01:43 +0200

    Anvil. Code refactoring in tizen.js driver config.

--- tests/anvil/driver/platforms/tizen.js
+++ tests/anvil/driver/platforms/tizen.js
@@ -1,39 +1,35 @@
-/*
- * Appcelerator Titanium Mobile
+/* Appcelerator Titanium Mobile
  * Copyright (c) 2011-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  *
  * Purpose: contains specific logic for running driver commands on Tizen
  *
- * Description: contains Tizen specific wrapper functions around common driver commands
- */
+ * Description: contains Tizen specific wrapper functions around common driver commands */
 
 var path = require("path"),
 	net = require("net"),
-	http = require('http'),
-	fs = require('fs'),
+	http = require("http"),
+	fs = require("fs"),
 	os = require("os"),
 	common = require(path.resolve(driverGlobal.driverDir, "common")),
 	driverUtils = require(path.resolve(driverGlobal.driverDir, "driverUtils"));
 
 module.exports = new function() {
-	var self = this;
-	var commandFinishedCallback;
-	var testPassFinishedCallback;
-	var server;
-	var serverRunning = false;
-	var serverListening = true;
-	var browserOnlyMode = false;
-	var connection;
-	var stoppingHarness = false;
+	var self = this,
+		commandFinishedCallback,
+		testPassFinishedCallback,
+		server,
+		serverRunning = false,
+		serverListening = true,
+		connection,
+		stoppingHarness = false;
 
 	this.name = "tizen";
 
 	this.init = function(commandCallback, testPassCallback) {
-		// check mobile web specific config items
+		// Check mobile web specific config items
 		driverUtils.checkConfigItem("httpPort", driverGlobal.config.httpPort, "number");
-
 		commandFinishedCallback = commandCallback;
 		testPassFinishedCallback = testPassCallback;
 	};
@@ -42,16 +38,9 @@ module.exports = new function() {
 		var commandElements = command.split(" ");
 
 		if (commandElements[0] === "start") {
-			var browserOnlyArg = driverUtils.getArgument(commandElements, "--browser-only");
-			if (browserOnlyArg === "true") {
-				browserOnlyMode = browserOnlyArg;
-			}
-
 			common.startTestPass(commandElements, self.startConfig, commandFinishedCallback);
-
 		} else if (commandElements[0] === "exit") {
 			process.exit(1);
-
 		} else {
 			driverUtils.log("invalid command\n\n"
 				+ "Commands:\n"
@@ -60,11 +49,7 @@ module.exports = new function() {
 				+ "            --config-set=<config set ID> - runs the specified config set\n"
 				+ "            --config=<config ID> - runs the specified configuration only\n"
 				+ "            --suite=<suite name> - runs the specified suite only\n"
-				+ "            --test=<test name> - runs the specified test only (--suite must be specified)\n"
-				+ "            --browser-only=<boolean> - if set to true, the harness is not deployed to a device\n"
-				+ "                and instead the user is expected to manually load the \"127.0.0.1/index.html\"\n"
-				+ "                url in order to run tests.  Reloading the page manually will be required when\n"
-				+ "                the harness is restarted due to a config finishing or an error occurring\n\n"
+				+ "            --test=<test name> - runs the specified test only (--suite must be specified)\n\n"
 				+ "    exit - exit driver\n",
 				0, true);
 
@@ -75,8 +60,8 @@ module.exports = new function() {
 	var createHarness = function(successCallback, errorCallback) {
 		var argString = "harness com.appcelerator.harness " + path.resolve(driverGlobal.harnessDir, "tizen tizen") + " " + driverGlobal.config.currentTiSdkDir;
 
-		// due to python behavior on windows, we need to escape the slashes in the argument string
-		if (os.platform().substr(0 ,3) === "win") {
+		// Due to python behavior on windows, we need to escape the slashes in the argument string
+		if (os.platform().substr(0, 3) === "win") {
 			argString = argString.replace(/\\/g, "\\\\");
 		}
 
@@ -85,7 +70,7 @@ module.exports = new function() {
 			"\"" + path.resolve(driverGlobal.config.currentTiSdkDir, "project.py") + "\" " + argString,
 			successCallback,
 			errorCallback
-			);
+		);
 	};
 
 	var deleteHarness = function(callback) {
@@ -96,6 +81,7 @@ module.exports = new function() {
 	var buildHarness = function(successCallback, errorCallback) {
 		var buildCallback = function() {
 			var  command_str = "titanium build --platform=tizen --project-dir " + path.resolve(driverGlobal.harnessDir, "tizen", "harness");
+
 			driverUtils.runCommand(command_str, driverUtils.logStdout, function(error) {
 				if (error !== null) {
 					driverUtils.log("error encountered when building harness: " + error);
@@ -109,7 +95,6 @@ module.exports = new function() {
 
 		if (path.existsSync(path.resolve(driverGlobal.harnessDir, "tizen", "harness", "tiapp.xml"))) {
 			buildCallback();
-			
 		} else {
 			driverUtils.log("harness does not exist, creating");
 			createHarness(buildCallback, errorCallback);
@@ -117,35 +102,33 @@ module.exports = new function() {
 	};
 	
 	this.startConfig = function() {
-		
 		var deleteCallback = function() {
-			deleteHarness(uninstallCallback);
-		};
-		
-		var uninstallCallback = function() {
-			uninstallHarness(buildCallback, commandFinishedCallback);
-		};
-		
-		var buildCallback = function() {
-			buildHarness(serverCallback, commandFinishedCallback);
-		};
-		
-		var serverCallback = function() {
-			startServer(runCallback, commandFinishedCallback);
-		};
-		
-		var runCallback = function() {
-			runHarness(null, commandFinishedCallback);
-		};
+				deleteHarness(uninstallCallback);
+			},
+			uninstallCallback = function() {
+				uninstallHarness(buildCallback, commandFinishedCallback);
+			},
+			buildCallback = function() {
+				buildHarness(serverCallback, commandFinishedCallback);
+			},
+			serverCallback = function() {
+				startServer(runCallback, commandFinishedCallback);
+			},
+			runCallback = function() {
+				runHarness(null, commandFinishedCallback);
+			};
 				
-		// this is the same whether browser only mode is enabled or not
+		// This is the same whether browser only mode is enabled or not
 		common.customTiappXmlProperties["driver.httpPort"] = {value: driverGlobal.config.httpPort, type: "int"};
 
 		function getIpAddress() {
-			var networkInterfaces = require("os").networkInterfaces();
+			var networkInterfaces = os.networkInterfaces(),
+				address;
+
 			for (i in networkInterfaces) {
 				for (j in networkInterfaces[i]) {
-					var address = networkInterfaces[i][j];
+					address = networkInterfaces[i][j];
+
 					if (address.family === 'IPv4' && !(address.internal)) {
 						return address.address;
 					}
@@ -153,22 +136,20 @@ module.exports = new function() {
 			}
 		}
 				
-		// get the address for the driver that should be accessible to the harness via wifi
+		// Get the address for the driver that should be accessible to the harness via wifi
 		var ipAddress = getIpAddress();
+
 		if (ipAddress) {
 			driverGlobal.httpHost = "http://" + ipAddress;
-			/*
-			make sure that the harness has the correct wifi address to use when making requests to 
-			the driver
-			*/
-			common.customTiappXmlProperties["driver.httpHost"] = {value: driverGlobal.httpHost, type: "string"};
 
+			// Make sure that the harness has the correct wifi address to use when making requests to the driver
+			common.customTiappXmlProperties["driver.httpHost"] = { value: driverGlobal.httpHost, type: "string" };
 		} else {
 			driverUtils.log("unable to get IP address", driverGlobal.logLevels.quiet);
 			commandFinishedCallback();
 		}
 				
-		//FIRE 
+		// Fire
 		self.deviceIsConnected(function(connected) {
 			if (connected) {
 				common.startConfig(deleteCallback);
@@ -177,19 +158,18 @@ module.exports = new function() {
 				commandFinishedCallback();
 			}
 		});
-		
 	};
 	
 	var startServer = function(successCallback, errorCallback) {
-		server = http.createServer(function (request, response) {
-			var prefix = path.resolve(driverGlobal.harnessDir, "tizen", "harness", "build", "tizen");
-			var filePath = prefix + request.url.split("?")[0];
-			if (filePath === prefix + '/') {
-				filePath = prefix + '/index.html';
-			}
+		server = http.createServer(function(request, response) {
+			var prefix = path.resolve(driverGlobal.harnessDir, "tizen", "harness", "build", "tizen"),
+				filePath = prefix + request.url.split("?")[0],
+				contentType = 'text/html',
+				postData = '';
+
+			(filePath === prefix + '/') && (filePath = prefix + '/index.html');
 
 			var extname = path.extname(filePath);
-			var contentType = 'text/html';
 			switch (extname) {
 				case '.js':
 					contentType = 'text/javascript';
@@ -197,7 +177,6 @@ module.exports = new function() {
 
 				case '.css':
 					contentType = 'text/css';
-					break;
 			}
 
 			if (extname !== ".anvil") {
@@ -207,63 +186,53 @@ module.exports = new function() {
 							if (error) {
 								response.writeHead(500);
 								response.end();
-
 							} else {
 								response.writeHead(200, {
 									"Content-Type": contentType,
 									"Cache-Control": "max-age=0, must-revalidate"
 								});
+
 								response.end(content, 'utf-8');
 							}
 						});
-
 					} else {
 						response.writeHead(404);
 						response.end();
 					}
 				});
-
 			} else {
-		        var postData = '';
-
-    		    request.on('data', function (data) {
-    		        postData += data;
+				request.on('data', function(data) {
+					postData += data;
 				});
-    		    request.on('end', function () {
+				request.on('end', function() {
 					var responseData = common.processHarnessMessage(postData);
-
+					
 					response.writeHead(200, {
 						"Content-Type": contentType,
 						"Cache-Control": "max-age=0, must-revalidate"
 					});
 					response.end(responseData, 'utf-8');
-        		});
+				});
 			}
 		});
 
-		server.on('error', function (e) {
-			if ((e.code === 'EADDRINUSE') && (serverRunning === false)) {
+		server.on('error', function(e) {
+			if (e.code === 'EADDRINUSE' && serverRunning === false) {
 				driverUtils.log('Address in use, retrying...');
 				setTimeout(function() {
-					if (serverListening === true) {
-						server.close();
-					}
+					(serverListening === true) && server.close();
 					serverListening = true;
 					server.listen(driverGlobal.config.httpPort);
 				}, 2000);
-
 			} else {
 				errorCallback();
 			}
 		});
 
-		server.on('listening', function (e) {
+		server.on('listening', function(e) {
 			serverRunning = true;
 			driverUtils.log("Server running at " + driverGlobal.httpHost + ":" + driverGlobal.config.httpPort);
-
-			if (successCallback !== null) {
-				successCallback();
-			}
+			successCallback && successCallback();
 		});
 
 		serverListening = true;
@@ -271,48 +240,34 @@ module.exports = new function() {
 	};
 	
 	var runHarness = function(successCallback, errorCallback) {
-		process.chdir( path.resolve(driverGlobal.harnessDir, "tizen/harness/build/tizen") );
+		process.chdir(path.resolve(driverGlobal.harnessDir, "tizen/harness/build/tizen"));
 		driverUtils.runCommand("web-run -w tizenapp.wgt -i http://www.test.com",
+
 		driverUtils.logStdout, function(error) {
 			if (error !== null) {
 				driverUtils.log("error encountered when running harness: " + error);
-				if (errorCallback) {
-					errorCallback();
-				}
-			}else{
+				errorCallback && errorCallback();
+			} else {
 				driverUtils.log('installed, running...');
-				if (successCallback)
-					successCallback();
+				successCallback && successCallback();
 			}
-			
 		});
 	};
 	
 	var uninstallHarness = function(successCallback, errorCallback) {
 		driverUtils.runCommand("web-uninstall -i http://www.test.com", driverUtils.logStdout, function(error) {
 			if (error !== null) {
-				driverUtils.log("error encountered when uninstalling harness: " + error);
-				if (errorCallback) {
-					errorCallback();
-				}
-
+				driverUtils.log("Error encountered when uninstalling harness: " + error);
+				errorCallback && errorCallback();
 			} else {
-				driverUtils.log("harness uninstalled");
-				if (successCallback) {
-					successCallback();
-				}
+				driverUtils.log("Harness uninstalled");
+				successCallback && successCallback();
 			}
 		});
 	};
 
-	// handles restarting the test pass (usually when an error is encountered)
+	// Handles restarting the test pass (usually when an error is encountered)
 	this.resumeConfig = function() {
-		
-		if (browserOnlyMode === "true") {
-			// in browser only mode, no action is required
-			return;
-		}
-
 		var runCallback = function() {
 			runHarness(commandFinishedCallback);
 		};
@@ -321,7 +276,7 @@ module.exports = new function() {
 		startServer(runCallback, commandFinishedCallback);
 	};
 
-	// called when a config is finished running
+	// Called when a config is finished running
 	this.finishConfig = function() {
 		stopHarness();
 		common.finishConfig(testPassFinishedCallback);
@@ -337,20 +292,13 @@ module.exports = new function() {
 	
 	this.deviceIsConnected = function(callback) {
 		driverUtils.runCommand("sdb devices", driverGlobal.logLevels.quiet, function(error, stdout, stderr) {
-			var searchString = "List of devices attached";
-			var deviceListString = "";
-			var startPos = stdout.indexOf(searchString);
-			if (startPos > -1) {
-				var deviceListString = stdout.substring(startPos + searchString.length, stdout.length);
-				deviceListString = deviceListString.replace(/\s/g,"");
-			}
-
-			if (deviceListString.length < 1) {
-				callback(false);
-
-			} else {
-				callback(true);
-			}
+			var searchString = "List of devices attached", 
+				deviceListString = "",
+				startPos = stdout.indexOf(searchString);
+			
+			//Parse and check
+			startPos > -1 && (deviceListString = stdout.substring(startPos + searchString.length, stdout.length).replace(/\s/g,""));
+			callback(deviceListString.length > 1);
 		});
 	};
 };
