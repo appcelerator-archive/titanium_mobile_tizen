7dc56f1c432b89624cea4df822c8397e92c7f96d
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-02-27 16:06:22 +0200

    Add Bluetooth, NFC, RangeFilter and SimpleCoordinates wrappers

---
+++ titanium/Ti/Tizen/NFC/NDEFMessage.js
@@ -0,0 +1,42 @@
+define(['Ti/_/declare'], function(declare) {
+	return declare('Ti.Tizen.NFC.NDEFMessage', null, {
+		constructor: function(args) {
+			if(args.toString() === '[object NDEFMessage]') {
+				this._obj = args;
+			} else {
+				if ('rawData' in args) {
+					this._obj = new tizen.NDEFMessage(args.rawData);
+				} else if ('ndefRecords' in args) {
+					this._obj = new tizen.NDEFMessage(args.ndefRecords);
+				} else if () {
+					this._obj = new tizen.NDEFMessage();
+				} else {
+					Ti.API.error('Constructor with given parameters doesn\'t exists');
+				}
+			}
+		},
+
+		constants: {
+			recordCount: {
+				get: function() {
+					return this._obj.recordCount;
+				}
+			},
+		},
+
+		properties: {
+			records: {
+				get: function() {
+					return this._obj.records;
+				},
+				set: function(value) {
+					this._obj.records = value;
+				}
+			},
+		},
+
+		toByte: function() {
+			return this._obj.toByte();
+		}
+	});
+});
