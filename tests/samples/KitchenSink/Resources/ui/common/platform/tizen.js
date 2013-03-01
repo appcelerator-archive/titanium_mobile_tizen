5f681c982146597ec3009bd7c920e70745074694
Stanislav Fedorenko <stanislav.fedorenko@globallogic.com>
2013-03-01 11:52:27 +0200

    bt 2.0

--- tests/samples/KitchenSink/Resources/ui/common/platform/tizen.js
+++ tests/samples/KitchenSink/Resources/ui/common/platform/tizen.js
@@ -8,6 +8,7 @@ function tizen(_args) {
 			{title: 'Download demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizenDownload'},
 			{title: 'System info and Power', hasChild: true, test: 'ui/handheld/tizen/platform/tizenSystemInfo'},
 			{title: 'NFC demo', hasChild: true, test: 'ui/handheld/tizen/platform/tizenNFC'},
+			{title: 'Bluetooth demo', hasChild: true, test: 'ui/handheld/tizen/platform/bluetooth'},
 			{title: 'Messaging', hasChild: true, test: 'ui/handheld/tizen/platform/messaging/messaging'},
 			{title: 'Media content', hasChild: true, test: 'ui/handheld/tizen/platform/media_content'},
 			{title: 'Alarm', hasChild: true, test: 'ui/handheld/tizen/platform/alarm'},
