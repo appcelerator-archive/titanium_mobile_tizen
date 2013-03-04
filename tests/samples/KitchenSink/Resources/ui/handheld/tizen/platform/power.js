070f8ee90e3f5deb70e14d4288f46b91ac7bfd72
ivan.moroz <ivan.moroz@globallogic.com>
2013-03-04 13:34:39 +0200

    Added separate test for power in KS

---
+++ tests/samples/KitchenSink/Resources/ui/handheld/tizen/platform/power.js
@@ -0,0 +1,19 @@
+function tizen_power() {
+	var win = Titanium.UI.createWindow(),
+		buttonTurnOffScreen = Ti.UI.createButton({
+			title: 'Turn Off Screen',
+			top: 10
+		});
+
+
+
+	buttonTurnOffScreen.addEventListener('click', function(){
+		//tizen.power.turnScreenOff();
+		Ti.Tizen.Power.turnScreenOff();
+	});
+
+	win.add(buttonTurnOffScreen);
+	return win;
+}
+
+module.exports = tizen_power;
