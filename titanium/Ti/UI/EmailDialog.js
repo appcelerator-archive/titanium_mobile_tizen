db32cb8df47b30e8e0ad1151f0f2419cb16d6b73
ivan.moroz <ivan.moroz@globallogic.com>
2013-03-12 15:01:43 +0200

    Added the supporting EmailDialog for new Tizen's API in KS

--- titanium/Ti/UI/EmailDialog.js
+++ titanium/Ti/UI/EmailDialog.js
@@ -4,53 +4,79 @@ define(["Ti/_", "Ti/_/declare", "Ti/_/Evented", "Ti/_/lang"],
 	return declare("Ti.UI.EmailDialog", Evented, {
 
 		open: function() {
-			var r = this.toRecipients || [],
-				url = "mailto:" + r.join(","),
-				i, j,
-				fields = {
-					subject: "subject",
-					ccRecipients: "cc",
-					bccRecipients: "bcc",
-					messageBody: "body"
+			var self = this,
+				fields = {//Dictionary between two API's
+					toRecipients: 'to',
+					ccRecipients: 'cc',
+					bccRecipients: 'bcc',
+					subject: 'subject',
+					messageBody: "text",
+					attachment: "attachments"
 				},
-				params = {};
+				appControl = new tizen.ApplicationControl('http://tizen.org/appcontrol/operation/compose',null,null,null,getAllParameters());
 
-			for (i in fields) {
-				if (j = this[i]) {
-					require.is(j, "Array") && (j = j.join(","));
-					params[fields[i]] = j;
+			//Create a parameter for one of field
+			function setParameter(name, valueArray){
+
+				require.is(valueArray, "Array") || (valueArray = [valueArray]);
+
+				if(valueArray.length > 0) {
+					return new tizen.ApplicationControlData(name, valueArray);
 				}
+			};
+
+			//create all parameters what was filled
+			function getAllParameters(){
+				var params = [];
+				for(i in fields) {
+					params.push(setParameter(fields[i], self[i]));
+				}
+				return params;
+			};
+
+			//Launching the native Tizen's message aplication
+			tizen.application.launchAppControl(
+				appControl,
+				null,
+				function() {Ti.API.info('Launch appControl succeeded')},
+				function(e) {Ti.API.info('Launch appControl failed. Reason: ' + e.name)}
+			);
+		},
+
+		addAttachment: function(blob){
+
+			require.is(this.attachment, "Array") || (this.attachment = []);
+
+			if(blob.nativePath) {
+				this.attachment.push(blob.nativePath);
+			} else {
+				Ti.API.info('Tizen currently only supports attaching a blob with the path to physical file present on the Tizen device.')
 			}
-
-			this.html || params.body && (params.body = _.escapeHtmlEntities(params.body));
-			params = lang.urlEncode(params);
-
-			location.href = url + (params ? "?" + params : "");
-
-			this.fireEvent("complete", {
-				result: this.SENT,
-				success: true
-			});
 		},
 		
 		isSupported: function() {
 			return true;
 		},
 
+		setHtml: function() {
+			Ti.API.info('Tizen does not support the content of email as HTML');
+		},
+
 		constants: {
 			CANCELLED: 0,
 			FAILED: 3,
 			SAVED: 1,
-			SENT: 2
+			SENT: 2,
+			html: false
 		},
 
 		properties: {
 		    bccRecipients: void 0,
 		    ccRecipients: void 0,
-		    html: false,
 		    messageBody: void 0,
 		    subject: void 0,
-		    toRecipients: void 0
+		    toRecipients: void 0,
+			attachment: void 0
 		}
 
 	});
