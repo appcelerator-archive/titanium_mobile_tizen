88183f2e0438353f9986e4ce89b3793b34b25a80
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-04-01 16:14:34 +0300

    Fix Ti.UI.EmailDialog according to code conventions

--- titanium/Ti/UI/EmailDialog.js
+++ titanium/Ti/UI/EmailDialog.js
@@ -1,10 +1,10 @@
-define(["Ti/_/declare", "Ti/_/UI/MessagingDialog"], function (declare, MessagingDialog) {
-		return declare("Ti.UI.EmailDialog", MessagingDialog, {
+define(['Ti/_/declare', 'Ti/_/UI/MessagingDialog'], function (declare, MessagingDialog) {
+		return declare('Ti.UI.EmailDialog', MessagingDialog, {
 			constants: {
 				// The id of Tizen application service that will provide the email dialog
-				// TODO: according to documentation here should be alias "tizen.messages" 
+				// TODO: according to documentation here should be alias 'tizen.messages' 
 				// https://developer.tizen.org/help/index.jsp?topic=%2Forg.tizen.native.appprogramming%2Fhtml%2Fguide%2Fapp%2Fappcontrol_message.htm
-				//but it does not work on available devices and emulator. Check it once more next Tizen version release
+				// but it does not work on available devices and emulator. Check it once more next Tizen version release
 				_id: 'vxqbrefica.Email',
 
 				// A mapping between the name of the property and the corresponding parameter name for
@@ -14,8 +14,8 @@ define(["Ti/_/declare", "Ti/_/UI/MessagingDialog"], function (declare, Messaging
 					ccRecipients: 'cc',
 					bccRecipients: 'bcc',
 					subject: 'subject',
-					messageBody: "text",
-					attachment: "attachments"
+					messageBody: 'text',
+					attachment: 'attachments'
 				}
 			},
 
