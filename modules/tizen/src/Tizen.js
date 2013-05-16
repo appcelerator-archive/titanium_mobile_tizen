e0aa73ed40b2bbed588b348159829a71f8f1d54b
Igor Ostash <igor.ostash@globallogic.com>
2013-05-16 15:14:54 +0300

    Package module added

--- modules/tizen/src/Tizen.js
+++ modules/tizen/src/Tizen.js
@@ -2,9 +2,9 @@
 
 define(['Ti/_/lang', 'Ti/_/Evented', 'Tizen/_/AttributeFilter', 'Tizen/_/AttributeRangeFilter', 'Tizen/_/CompositeFilter', 'Tizen/_/SortMode',
 	'Tizen/_/SimpleCoordinates', 'Tizen/_/Power', 'Tizen/_/Calendar', 'Tizen/_/Download', 'Tizen/_/Alarm', 'Tizen/_/CallHistory', 'Tizen/_/Notification', 'Tizen/_/Apps',
-	'Tizen/_/SystemSetting', 'Tizen/_/Bluetooth', 'Tizen/_/Messaging', 'Tizen/_/SystemInfo', 'Tizen/_/NFC', 'Tizen/_/Contact/ContactRef', 'Tizen/_/Bookmark'],
+	'Tizen/_/SystemSetting', 'Tizen/_/Bluetooth', 'Tizen/_/Messaging', 'Tizen/_/SystemInfo', 'Tizen/_/NFC', 'Tizen/_/Contact/ContactRef', 'Tizen/_/Bookmark', 'Tizen/_/Package'],
 	function(lang, Evented, AttributeFilter, AttributeRangeFilter, CompositeFilter, SortMode, SimpleCoordinates, Power,
-			 Calendar, Download, Alarm, CallHistory, Notification, Apps, SystemSetting, Bluetooth, Messaging, SystemInfo, NFC, ContactRef, Bookmark) {
+			 Calendar, Download, Alarm, CallHistory, Notification, Apps, SystemSetting, Bluetooth, Messaging, SystemInfo, NFC, ContactRef, Bookmark, Package) {
 
 		var Tizen = lang.mixProps(require.mix({}, Evented), {
 
@@ -55,7 +55,9 @@ define(['Ti/_/lang', 'Ti/_/Evented', 'Tizen/_/AttributeFilter', 'Tizen/_/Attribu
 				Messaging: Messaging,
 				SystemInfo: SystemInfo,
 				NFC: NFC,
-				Bookmark: Bookmark
+				Bookmark: Bookmark,
+				Package: Package
+
 			}
 
 		}, true);
