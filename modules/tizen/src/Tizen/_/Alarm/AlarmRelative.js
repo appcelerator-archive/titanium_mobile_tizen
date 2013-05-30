37db4a9ae7c5fc97c50d34447d8ee788029fc007
Igor Ostash <igor.ostash@globallogic.com>
2013-05-30 17:34:17 +0300

    Alarm Relative wraper constructor fixed

--- modules/tizen/src/Tizen/_/Alarm/AlarmRelative.js
+++ modules/tizen/src/Tizen/_/Alarm/AlarmRelative.js
@@ -12,7 +12,7 @@ define(['Ti/_/declare', 'Tizen/_/Alarm/Alarm'], function(declare, Alarm) {
 				// Check if the required parameters are present (do not check for the optional ones).
 				if('delay' in args) {
 					// args is a dictionary that the user of the wrapper module passed to the creator function.
-					this._obj = new tizen.AlarmRelative(args.delay, args.period);
+					this._obj = args.period ? new tizen.AlarmRelative(args.delay, args.period) : new tizen.AlarmRelative(args.delay);
 				} else {
 					throw new Error('Constructor with given parameters doesn\'t exist');
 				}
