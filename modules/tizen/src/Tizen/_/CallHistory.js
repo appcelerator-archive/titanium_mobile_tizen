dcf3ae03fc33ea4ef89f748902ca78b9121009da
Igor Ostash <igor.ostash@globallogic.com>
2013-06-05 16:17:05 +0300

    Tizen 2.1 wrapper: fixing optional parameters: Callhistory

--- modules/tizen/src/Tizen/_/CallHistory.js
+++ modules/tizen/src/Tizen/_/CallHistory.js
@@ -22,23 +22,33 @@ define(['Ti/_/lang', 'Tizen/_/CallHistory/CallHistoryEntry', 'Ti/_/Evented'], fu
 	return lang.mixProps(require.mix({}, Evented), {
 
 			find: function(callback, filter /*AbstractFilter*/, sortMode /*SortMode*/, limit /*unsigned long*/, offset /*unsigned long*/) {
-				tizen.callhistory.find(callback && function(entries) {
-					var result = [],
-						entriesCount = entries.length,
-						i = 0;
+				// Tizen distinguishes between undefined optional parameters (this gives an error) and missing optional parameters (this is correct).
+				var args = [
+					function(entries) {
+						var result = [],
+							entriesCount = entries.length,
+							i = 0;
 
-					for (; i < entriesCount; i++) {
-						result.push(new CallHistoryEntry(entries[i]));
+						for (; i < entriesCount; i++) {
+							result.push(new CallHistoryEntry(entries[i]));
+						}
+
+						callback({
+							code: 0,
+							success: true,
+							entries: result
+						});
+					},
+					function(e) {
+						onError(e, callback);
 					}
+				];
+				(typeof filter !== 'undefined') && args.push((filter && filter._obj) || filter);
+				(typeof sortMode && sortMode._obj) && args.push((sortMode && sortMode._obj) || sortMode);
+				(typeof limit !== 'undefined') && args.push(limit);
+				(typeof offset !== 'undefined') && args.push(offset);
 
-					callback({
-						code: 0,
-						success: true,
-						entries: result
-					});
-				}, callback && function(e) {
-					onError(e, callback);
-				}, filter && filter._obj, sortMode && sortMode._obj, limit, offset);
+				tizen.callhistory.find.apply(tizen.callhistory, args);
 			},
 
 			remove: function(entry /*CallHistoryEntry*/) {
@@ -46,27 +56,38 @@ define(['Ti/_/lang', 'Tizen/_/CallHistory/CallHistoryEntry', 'Ti/_/Evented'], fu
 			},
 
 			removeBatch: function(entries /*CallHistoryEntry*/, callback) {
+				// Tizen distinguishes between undefined optional parameters (this gives an error) and missing optional parameters (this is correct).
 				var i = 0,
 					entriesCount = entries.length,
-					result = [];
+					result = [],
+					args = [];
 
 				for (; i < entriesCount; i++) {
 					result.push(entries[i]._obj);
 				}
 
-				tizen.callhistory.removeBatch(result, callback && function () {
-					onSuccess(callback);
-				}, callback && function(e) {
-					onError(e, callback);
-				});
+				args.push(result);
+				(typeof callback !== 'undefined') && args.push(function () {
+						onSuccess(callback);
+					},
+					function(e) {
+						onError(e, callback);
+					}
+				);
+				tizen.callhistory.removeBatch.apply(tizen.callhistory, args);
 			},
 
 			removeAll: function(callback) {
-				tizen.callhistory.removeAll(callback && function () {
-					onSuccess(callback);
-				}, callback && function(e) {
-					onError(e, callback);
-				});
+				// Tizen distinguishes between undefined optional parameters (this gives an error) and missing optional parameters (this is correct).
+				var args = [];
+				(typeof callback !== 'undefined') && args.push(function () {
+						onSuccess(callback);
+					},
+					function(e) {
+						onError(e, callback);
+					}
+				);
+				tizen.callhistory.removeAll.apply(tizen.callhistory, args);
 			},
 
 			addEventListener: function () {
